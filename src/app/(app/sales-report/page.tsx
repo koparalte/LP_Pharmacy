25b54fec0350a2ccb2f16810450230e0785830af
Kopa Ralte <kopa.ralte@gmail.com>
2025-06-18 14:37:07 +0000

    remove all code connected to this Error: SyntaxError: Invalid regular ex

--- src/app/(app/sales-report/page.tsx
+++ src/app/(app/sales-report/page.tsx
@@ -1,4 +1,5 @@
 // This file path is problematic and causes Next.js routing errors
 // due to the folder structure "(app/sales-report)".
-// The Sales Report feature has been removed. This file is kept empty.
+// All sales report functionality has been removed. This file is kept empty
+// to prevent Next.js from attempting to process it as a page from this malformed path.
 export {};
